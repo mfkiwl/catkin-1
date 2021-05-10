eda8a31673973e21aa2a6b21de993c553d79d51f
wws <baaixw@qq.com>
2021-05-10 09:53:24 +0800

    original VINS

---
+++ openbag.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+#rosbag play -r 1 /home/wws/Downloads/Kowloonton/2019-05-30-16-02-14.bag --clock   # 
+# rosbag play -r 0.5 /home/wws/JianDong/2019-04-28-20-58-02.bag   # 
+#rosbag play -r 1 /home/wws/Downloads/cooperativeMappingData/data20191023Whampo/2019-10-23-11-23-10.bag --clock 
+
+rosbag  play -r 1  /home/wws/JianDong/ 2021-04-13-19-22-42.bag 
